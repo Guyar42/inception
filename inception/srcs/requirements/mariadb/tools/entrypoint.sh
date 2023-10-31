a1a6c50c6063c5cb3ab290d55614fa78a270d433
Mohamadou-Boiguille <64551781+Mohamadou-Boiguille@users.noreply.github.com>
2023-10-31 20:08:00 +0100

    add entrypoint root

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -9,6 +9,9 @@ else
 	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 	mysql -uroot -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 	mysql -uroot -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';"
+	mysql -uroot -e "UPDATE mysql.user SET Host='localhost' WHERE User='root';"
+	mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
+
 	mysql -uroot -e "FLUSH PRIVILEGES;"
 	
 fi
