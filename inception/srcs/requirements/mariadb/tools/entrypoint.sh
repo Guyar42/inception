e77680667138a61303a23d95c3e11996712d9aa0
inception <inception@inception.iince>
2023-10-31 14:14:15 -0400

    update..

---
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+service mariadb start
+
+if [ -d "/var/lib/mysql/$SQL_DATABASE" ]
+then
+	echo "Database already exists"
+else
+	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
+	mysql -uroot -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+	mysql -uroot -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';"
+	mysql -uroot -e "FLUSH PRIVILEGES;"
+	
+fi
+
+service mariadb stop
+
+exec "$@"
